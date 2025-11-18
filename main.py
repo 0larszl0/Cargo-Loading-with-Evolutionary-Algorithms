ecd8274d63bc345ff40dc2e28d930fbbc9e90e81
0larszl0 <oliver.winhammar@gmail.com>
2025-11-18 15:36:55 +0000

    Misconception of the population vs the sample of circles to pack. That should be resolved now, or at least started to be.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 import random
 
 random.seed(42)
@@ -5,6 +6,9 @@ random.seed(42)
 # -- Initialise container types -- #
 # Define unit container size
 CONTAINER_SIZE = (20., 15.)
+
+# Define the number of sides a cylinder will have
+CYLINDER_SIDES = 8
 
 # Define cylinder containers
 CYLINDERS = [
@@ -28,18 +32,28 @@ class Individual:
 class Population:
     """Manages a population of individuals and evolutionary operations."""
 
-    def __init__(self, size: int, num_cylinders: int, mutation_rate: float, max_generations: int, cylinder_sides: int = 8):
+    def __init__(self, size: int, num_cylinders: int, mutation_rate: float, max_generations: int, cylinder_sides: int):
         self.__size = size
         self.__num_cylinders = num_cylinders
         self.__mutation_rate = mutation_rate
         self.__max_generations = max_generations
+        self.__cylinder_sides = cylinder_sides
 
         self.__generations = 0
+        self.__population = []
 
-        # Initialises population by selecting a random variety of different cylinders, and saving them into objects
-        self.__population = [Individual(cylinder_sides, diameter / 2, weight) for weight, diameter in random.choices(CYLINDERS, k=size)]
-        print(self.__population)
+    def create_random_positions(self) -> List[int]:
+        """
+        Creates a random list of positions for each circle, except the first circle as that starts at the centre of the
+        container.
+        :return: List[int], a list of position numbers.
+        """
+        return random.sample(range(self.__num_cylinders * self.__cylinder_sides), k=self.__num_cylinders - 1)
 
 
 if __name__ == "__main__":
-    Population(50, 10, .1, 100)
+    cylinder_len = 5
+    sample_cylinders = [Individual(CYLINDER_SIDES, diameter / 2, weight) for weight, diameter in random.choices(CYLINDERS, k=cylinder_len)]
+
+    population = Population(50, cylinder_len, .1, 100, CYLINDER_SIDES)
+    print(population.create_random_positions())
