699903d6bb90d1494c971de9b75e96507ed92df3
0larszl0 <oliver.winhammar@gmail.com>
2025-11-19 18:19:29 +0000

    Moved decoding and feasibility checking into its own object. I didn't think it was right to have the population handling all that.
    This grouping method can be more flexible for things like visualisations, while being able to isolate each group of position numbers to themselves.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CONTAINER_WIDTH, CONTAINER_HEIGHT = (20., 15.)
 # Define the number of sides a cylinder will have
 CYLINDER_SIDES = 8
 
-# Define cylinder containers
+# Define cylinder containers: [(Weight, Diameter)]
 CYLINDERS = [
     (2500, 2.),  # Heavy tank
     (800, 1.5),  # Medium drum
@@ -20,8 +20,8 @@ CYLINDERS = [
 ]
 
 
-class Individual:
-    """Represents a cylinder of a particular type in this problem."""
+class Cylinder:
+    """Represents a cylinder of a particular type."""
 
     def __init__(self, sides: int, radius: float, weight: int):
         self.__sides = sides
@@ -59,71 +59,46 @@ class Individual:
         return self.__centre[1] - self.__radius
 
 
-class Population:
-    """Manages a population of individuals and evolutionary operations inside a container."""
+class CylinderGroup:
+    """Contains cylinders in a particular grouping."""
 
-    def __init__(self, size: int, num_cylinders: int, mutation_rate: float, max_generations: int, cylinder_sides: int,
-                 max_weight):
-        # - Initialise passed arguments - #
-        self.__size = size
+    def __init__(self, cylinders: List[Cylinder], num_cylinders: int, cylinder_sides: int, max_weight: int):
+        self.__cylinders = cylinders
         self.__num_cylinders = num_cylinders
-        self.__mutation_rate = mutation_rate
-        self.__max_generations = max_generations
         self.__cylinder_sides = cylinder_sides
         self.__max_weight = max_weight
 
-        self.__generations = 0
         self.__weight = 0
 
-        # - Initialise cylinders - #
-        # Get a random selection of different cylinder types and save them as objects
-        self.__cylinders = [Individual(CYLINDER_SIDES, diameter / 2, weight) for weight, diameter in random.choices(CYLINDERS, k=num_cylinders)]
+        # A group will contain a list of random position numbers for each cylinder, apart from the first as that is
+        # to be placed in the centre of the container.
+        self.__group = random.sample(range(num_cylinders * cylinder_sides), k=num_cylinders - 1)
 
-        # Sorts the cylinders in descending order based on size (radius)
-        self.__cylinders = sorted(self.__cylinders, reverse=True, key=lambda x: x.weight)
-
-        # Sets the first cylinder's centre to the middle of the container.
-        self.__cylinders[0].centre = (CONTAINER_WIDTH / 2, CONTAINER_HEIGHT / 2)
-
-        print("+-----------\tInitialised cylinders\t-----------+")
-        for i, cylinder in enumerate(self.__cylinders):
-            print(f"Cylinder {i+1}:\t- Weight: {cylinder.weight}\t- Centre: {cylinder.centre}\t- Radius: {cylinder.radius}")
-
-        # - Initialise population with random position strings - #
-        self.__population = [self.create_random_positions() for _ in range(size)]
-        print(f"\nSample of population: {random.sample(self.__population, k=5)}\n")
-
-    def create_random_positions(self) -> List[int]:
+    def decode(self, debug: bool = False) -> None:
         """
-        Creates a random list of positions for each circle, except the first circle as that starts at the centre of the
-        container.
-        :return: List[int], a list of position numbers.
+        Decodes the position numbers within the group.
+        :param bool debug: Whether to show debug messages or not.
+        :return: None
         """
-        return random.sample(range(self.__num_cylinders * self.__cylinder_sides), k=self.__num_cylinders - 1)
+        cprint(debug, f"Outputting decoding process for: {self.__group}")
 
-    def decode_positions(self, positions, debug: bool = False):
-        """
-        Decodes the position values and determines
-        :return:
-        """
-        cprint(debug, f"Outputting decoding process for: {positions}")
-
-        for i, position in enumerate(positions):
+        for i in range(self.__num_cylinders - 1):
             # Check if the position number is greater than the maximum position number for the ith circle being seen.
             max_positions = (i + 1) * self.__cylinder_sides
-            if position > max_positions:
-                # if it is reset the position to 0
-                position = 0
+            if self.__group[i] > max_positions:
+                # if it is reset the position number to 0
+                self.__group[i] = 0
 
-            cprint(debug, f"+----\tWorking on cylinder: {i + 1}\t----+")
-            positions[i] = self.check_feasibility(position, self.__cylinders[i + 1], max_positions, max_positions, debug)
-            cprint(debug, f"Final position: {positions[i]}, with position: {self.__cylinders[i + 1].centre}\n")
+            cprint(debug, f"+----\tWorking on cylinder: {i + 1}/{self.__num_cylinders - 1}\t----+")
+            self.__group[i] = self.check_feasibility(self.__group[i], self.__cylinders[i + 1], max_positions, max_positions, debug)
+            cprint(debug, f"Final position: {self.__group[i]}, with position: {self.__cylinders[i + 1].centre}\n")
 
-        print(positions)
+        print(self.__group)
 
         # --- Filter any -1 positions --- #
 
-    def check_feasibility(self, position: int, cylinder: Individual, total_positions: int, positions_left: int, debug: bool = False) -> int:
+
+    def check_feasibility(self, position: int, cylinder: Cylinder, total_positions: int, positions_left: int, debug: bool = False) -> int:
         """
         Checks whether a cylinder will be placed at a feasible position.
         :param int position: The position that is being checked.
@@ -189,11 +164,48 @@ class Population:
 
         return position
 
+
+class Population:
+    """Manages a population of individuals and evolutionary operations inside a container."""
+
+    def __init__(self, size: int, num_cylinders: int, mutation_rate: float, max_generations: int, cylinder_sides: int,
+                 max_weight):
+        self.__size = size
+        self.__mutation_rate = mutation_rate
+        self.__max_generations = max_generations
+
+        self.__generations = 0
+
+        # - Initialise cylinders - #
+        # Get a random selection of different cylinder types and save them as objects
+        self.__cylinders = [Cylinder(CYLINDER_SIDES, diameter / 2, weight) for weight, diameter in random.choices(CYLINDERS, k=num_cylinders)]
+
+        # Sorts the cylinders in descending order based on size (radius)
+        self.__cylinders = sorted(self.__cylinders, reverse=True, key=lambda x: x.weight)
+
+        # Sets the first cylinder's centre to the middle of the container.
+        self.__cylinders[0].centre = (CONTAINER_WIDTH / 2, CONTAINER_HEIGHT / 2)
+
+        print("+-----------\tInitialised cylinders\t-----------+")
+        for i, cylinder in enumerate(self.__cylinders):
+            print(f"Cylinder {i+1}:\t- Weight: {cylinder.weight}\t- Centre: {cylinder.centre}\t- Radius: {cylinder.radius}")
+
+        # - Initialise population with random position strings - #
+        self.__population = [CylinderGroup(self.__cylinders, num_cylinders, cylinder_sides, max_weight) for _ in range(size)]
+        print(f"\nSample of population: {random.sample(self.__population, k=3)}\n")
+
+    def decode(self) -> None:
+        """
+        Decodes the population to ensure each group of cylinders is grouped feasibly.
+        :return: None
+        """
+        for i, cylinder_group in enumerate(self.__population):
+            cylinder_group.decode(i == 0)
+            break
+
     def evolve(self):
         """Run a single generation of the genetic algorithm."""
-        for i, position_num in enumerate(self.__population):
-            self.decode_positions(position_num, i == 0)
-            break
+        self.decode()
 
 
 
