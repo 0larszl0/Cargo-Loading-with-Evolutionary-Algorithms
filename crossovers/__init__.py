2f9c0b586184c508eced308772c58b24356bda7b
0larszl0 <oliver.winhammar@gmail.com>
2025-12-01 11:01:10 +0000

    Updated multi_point crossover to be more advanced

--- crossovers/__init__.py
+++ crossovers/__init__.py
@@ -1,7 +1,7 @@
 from .single_point import single_point_crossover
 from .davis_order import davis_order_crossover
+from .multi_point import multi_point_crossover
 from .uniform import uniform_crossover
-from .multi_point import *
 
-__all__ = ["single_point_crossover", "multi_point_crossover", "true_multi_point_crossover", "davis_order_crossover",
+__all__ = ["single_point_crossover", "multi_point_crossover", "davis_order_crossover",
            "uniform_crossover"]
--- crossovers/multi_point.py
+++ crossovers/multi_point.py
@@ -1,22 +1,35 @@
+from .single_point import single_point_crossover
 from random import sample, choice
 from typing import List
 
 
-def multi_point_crossover(group1: List[int], group2: List[int]) -> List[int]:
+def multi_point_crossover(group1: List[int], group2: List[int], crossovers: int) -> List[int]:
     """
-    Perform multi-point crossover between two groups and randomly choose one of the offsprings.
+    Performs multi-point crossover between two groups and randomly chooses one of the offsprings.
     :param List[int] group1: A list of position numbers.
     :param List[int] group2: A list of position numbers.
+    :param int crossovers: The number of crossover points to use.
     :return: List[int]
     """
-    random_points = sample(range(len(group1)), 2)
-    low_point, high_point = min(random_points), max(random_points)
+    if crossovers == 1:  # if the user selected their to be only one crossover point
+        return single_point_crossover(group1, group2)
 
-    return choice((
-        group1[:low_point] + group2[low_point:high_point] + group1[high_point:],
-        group2[:low_point] + group1[low_point:high_point] + group2[high_point:]
-    ))
+    # Get a list of 'crossover' amount of random points, in ascending order.
+    random_points = sorted(sample(range(len(group1)), k=crossovers))
+
+    for i, point in enumerate(random_points):
+        if (i % 2 == 0) and (i == len(random_points) - 1):
+            group1[point:], group2[point:] = group2[point:], group1[point:]
+
+        elif i % 2 == 0:
+            group1[point: random_points[i + 1]], group2[point: random_points[i + 1]] = group2[point: random_points[i + 1]], group1[point: random_points[i + 1]]
+
+    return choice((group1, group2))
 
 
-def true_multi_point_crossover(group1: List[int], group2: List[int], crossovers: int):
-    ...
+if __name__ == "__main__":
+    # When running directly comment out the relative import at the top, and uncomment the below
+    # from single_point import single_point_crossover
+
+    print(multi_point_crossover(list(range(10)), list(range(10, 20)), 1))
+    print(multi_point_crossover(list(range(10)), list(range(10, 20)), 4))
